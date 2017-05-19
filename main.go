e25c4972272ce9d7864563c0818467e8c36608a4
maknahar <maknahar@yahoo.in>
2017-05-19 13:11:34 +0530

    Added database

--- main.go
+++ main.go
@@ -4,16 +4,41 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/jackc/pgx"
 )
 
 func main() {
 	port := os.Getenv("PORT")
-
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
+	//DATABASE_URL := os.Getenv("DATABASE_URL")
+	//EstablishDBConnection(DATABASE_URL)
+
 	router := GetRouter()
 	log.Println("listening in port", port)
 	http.ListenAndServe(":"+port, router)
+}
+
+var conn *pgx.ConnPool
+
+// Establish a connection to DB
+func EstablishDBConnection(url string) error {
+	if conn == nil {
+		connConfig, err := pgx.ParseURI(url)
+		if err != nil {
+			log.Fatalf("Invalid Database URL, Err: %+v", err)
+			return err
+		}
+		poolConfig := pgx.ConnPoolConfig{ConnConfig: connConfig, MaxConnections: 5}
+		conn, err = pgx.NewConnPool(poolConfig)
+		if err != nil {
+			log.Fatalf("Unable to connect to Database, Err: %+v", err)
+			return err
+		}
+		log.Println("Successfully established database connection")
+	}
+	return nil
 }
